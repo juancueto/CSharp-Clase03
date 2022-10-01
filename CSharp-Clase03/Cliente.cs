4e776a36dd7331ae7a160e189a346f4d46746218
juancueto <juan.cueto@outlook.com>
2022-10-01 01:33:08 -0500

    Clase 03

---
+++ CSharp-Clase03/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CSharp_Clase03
+{
+    public struct Cliente
+    {
+        public string NombreCompleto { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+        public bool EsNuevo { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nombre: {NombreCompleto} \n" +
+                $"Telefono: {Telefono}\n" +
+                $"Direccion: {Direccion}\n" +
+                $"Email: {Email}\n" +
+                $"Cliente Nuevo?: {(EsNuevo? "Si":"No")}";
+        }
+    }
+}
+
---
+++ CSharp-Clase03/Program.cs
@@ -0,0 +1,13 @@
+﻿using CSharp_Clase03;
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Cliente obj = new Cliente();
+obj.NombreCompleto = "Juan C";
+obj.Direccion = "ghfhg hjgh jhgj jhg";
+obj.Email = "juan@mail.com";
+obj.EsNuevo = true;
+obj.Telefono = "+51 456 767 345";
+
+Console.WriteLine(obj);
